b84f93235a8766c108f5c71bb4836f0f4325e40b
kungua <maguahongkun@outlook.com>
2019-07-18 21:52:03 +0800

    完成 列表

--- src/components/Monitor.vue
+++ src/components/Monitor.vue
@@ -1,31 +1,89 @@
 <template>
-    <div>
+    <div class="monitor-content">
         Monitor
+        <el-table
+                class="lu-table"
+                :data="tableData"
+                :max-height="500"
+                style="width: 100%">
+            <el-table-column
+                    prop="username"
+                    label="姓名"
+                    width="180">
+
+            </el-table-column>
+            <el-table-column
+                    prop="address"
+                    label="地址">
+
+            </el-table-column>
+            <el-table-column
+                    prop="birthday"
+                    label="生日"
+                    width="180">
+
+            </el-table-column>
+            <el-table-column
+                    prop="gender"
+                    label="性别"
+                    width="180">
+
+            </el-table-column>
+            <el-table-column
+                    prop="hobby"
+                    label="爱好"
+                    width="180">
+
+            </el-table-column>
+            <el-table-column
+                    prop="state"
+                    label="求职状态"
+                    width="180">
+
+            </el-table-column>
+            <el-table-column
+                    prop="time"
+                    label="时间"
+                    width="180">
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 
 <script>
     import Request from '../util/request'
     export default {
+        data() {
+            return {
+                tableData: []
+            }
+        },
         mounted() {
             this._getData()
         },
         methods: {
             _getData() {
                 Request.ajax({
-                    // url: "/table/list",
-                    url: '/test/exception',
+                    url: "/table/list",
+                    // url: '/test/exception',
                     // method: 'post',
                     data: { params: { page: 1 } }
                 })
                     .then(response => {
-                        console.log(response)
+                        console.log(response.data.items)
+                        this.tableData = response.data.items
                     });
             }
         }
     }
 </script>
 
-<style>
+<style lang="scss" scoped>
+    .monitor-content {
+        .lu-table {
+            margin-top: 12px;
+            margin-left: 10px;
+        }
+    }
 
 </style>
