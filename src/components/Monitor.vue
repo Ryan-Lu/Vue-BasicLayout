86c45f19bc0513e53d31c6dc817da21cf26a20a2
kungua <maguahongkun@outlook.com>
2019-07-18 21:34:09 +0800

    完成请求层并实现了一个报错的例子

--- src/components/Monitor.vue
+++ src/components/Monitor.vue
@@ -5,9 +5,25 @@
 </template>
 
 <script>
-export default {
-
-}
+    import Request from '../util/request'
+    export default {
+        mounted() {
+            this._getData()
+        },
+        methods: {
+            _getData() {
+                Request.ajax({
+                    // url: "/table/list",
+                    url: '/test/exception',
+                    // method: 'post',
+                    data: { params: { page: 1 } }
+                })
+                    .then(response => {
+                        console.log(response)
+                    });
+            }
+        }
+    }
 </script>
 
 <style>
---
+++ src/util/request.js
@@ -0,0 +1,33 @@
+import axios from "axios"
+import { Message } from 'element-ui'
+
+export default class Axios {
+    static ajax(options) {
+        const baseURL = "https://easy-mock.com/mock/5d0342af599408638e6ad79c/raapi";
+        return new Promise((resolve, reject) => {
+            axios({
+                url: options.url,
+                method: options.method || "GET",
+                baseURL,
+                timeout: 5000,
+                params: (options.data && options.data.params) || ""
+            }).then(responseText => {
+                const {data, status} = responseText
+                if (status === 200) {
+                    // 业务
+                    if (data.code === 0) {
+                        resolve(data)
+                    } else {
+                        Message({
+                            message: data.msg,
+                            type: 'error',
+                            duration: 1500
+                        })
+                    }
+                } else {
+                    reject(data)
+                }
+            });
+        });
+    }
+}
