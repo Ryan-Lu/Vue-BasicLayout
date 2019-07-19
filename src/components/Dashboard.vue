f5e13c1386ed36bfaabd08288e17a00d3b3ffb13
Ryan-lu <229239531@qq.com>
2019-07-19 10:56:22 +0800

    优化 css 样式

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -5,41 +5,48 @@
       <span>{{getUserInfo.userName}}</span>
       <img class="top-avatar" :src="getUserInfo.avatarUrl" alt />
     </div>
-    <div>
-      <Card
-        :percent="percent1"
-        :title="title1"
-        :used="used1"
-        :total="total1"
-        color="linear-gradient(60deg, #20CFF1 0%, #1CD4F3 100%)"
-      ></Card>
-      <Card
-        :percent="percent2"
-        :title="title2"
-        :used="used2"
-        :total="total2"
-        color="linear-gradient(60deg, #475ED5 0%, #7655E7 100%)"
-      ></Card>
-      <Card
-        :percent="percent3"
-        :title="title3"
-        :used="used3"
-        :total="total3"
-        color="linear-gradient(60deg, #0B9CE8 0%, #185BCF 100%)"
-      ></Card>
-      <Card
-        :percent="percent4"
-        :title="title4"
-        :used="used4"
-        :total="total4"
-        color="linear-gradient(60deg, #E46941 0%, #fe5196 100%)"
-      ></Card>
+    <div class="cardLine">
+      <div class="cardBox">
+        <Card
+          :percent="percent1"
+          :title="title1"
+          :used="used1"
+          :total="total1"
+          color="linear-gradient(60deg, #20CFF1 0%, #1CD4F3 100%)"
+        ></Card>
+      </div>
+      <div class="cardBox">
+        <Card
+          :percent="percent2"
+          :title="title2"
+          :used="used2"
+          :total="total2"
+          color="linear-gradient(60deg, #475ED5 0%, #7655E7 100%)"
+        ></Card>
+      </div>
+      <div class="cardBox">
+        <Card
+          :percent="percent3"
+          :title="title3"
+          :used="used3"
+          :total="total3"
+          color="linear-gradient(60deg, #0B9CE8 0%, #185BCF 100%)"
+        ></Card>
+      </div>
+      <div class="cardBox">
+        <Card
+          :percent="percent4"
+          :title="title4"
+          :used="used4"
+          :total="total4"
+          color="linear-gradient(60deg, #E46941 0%, #fe5196 100%)"
+        ></Card>
+      </div>
     </div>
     <div class="second-title">
       <h1>IO Utilization</h1>
     </div>
-    <div class="line-chart" ref="chartDom">
-    </div>
+    <div class="line-chart" ref="chartDom"></div>
   </div>
 </template>
 
@@ -47,7 +54,6 @@
 import Card from "./Card";
 import { mapGetters } from "vuex";
 import echarts from "echarts";
-
 
 export default {
   components: {
@@ -79,193 +85,252 @@ export default {
   mounted() {
     const myChart = echarts.init(this.$refs.chartDom);
     var option = {
-    backgroundColor: '#2F4685',
-    tooltip: {
-        trigger: 'axis',
+      backgroundColor: "#2F4685",
+      tooltip: {
+        trigger: "axis",
         axisPointer: {
-            lineStyle: {
-                color: '#57617B'
-            }
+          lineStyle: {
+            color: "#57617B"
+          }
         }
-    },
-    legend: {
-        icon: 'rect',
+      },
+      legend: {
+        icon: "rect",
         itemWidth: 14,
         itemHeight: 5,
         itemGap: 13,
-        data: ['移动', '电信', '联通'],
-        right: '4%',
+        data: ["移动", "电信", "联通"],
+        right: "4%",
         textStyle: {
-            fontSize: 12,
-            color: '#F1F1F3'
+          fontSize: 12,
+          color: "#F1F1F3"
         }
-    },
-    grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
+      },
+      grid: {
+        left: "3%",
+        right: "4%",
+        bottom: "3%",
         containLabel: true
-    },
-    xAxis: [{
-        type: 'category',
-        boundaryGap: false,
-        axisLine: {
+      },
+      xAxis: [
+        {
+          type: "category",
+          boundaryGap: false,
+          axisLine: {
             lineStyle: {
-                color: '#A9A9A9',
-                opacity: 0.2,
+              color: "#A9A9A9",
+              opacity: 0.2
             }
+          },
+          data: [
+            "9:00",
+            "10:00",
+            "11:00",
+            "12:00",
+            "13:00",
+            "14:00",
+            "15:00",
+            "16:00",
+            "17:00",
+            "18:00",
+            "19:00",
+            "20:00"
+          ]
         },
-        data: ['9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00']
-    }, {
-        axisPointer: {
+        {
+          axisPointer: {
             show: false
-        },
-        axisLine: {
+          },
+          axisLine: {
             lineStyle: {
-                color: '#A9A9A9',
-                opacity: 0.2,
+              color: "#A9A9A9",
+              opacity: 0.2
             }
-        },
-        axisTick: {
+          },
+          axisTick: {
             show: false
-        },
+          },
 
-        position: 'bottom',
-        offset: 20,
-        
-    }],
-    yAxis: [{
-        type: 'value',
-        axisTick: {
+          position: "bottom",
+          offset: 20
+        }
+      ],
+      yAxis: [
+        {
+          type: "value",
+          axisTick: {
             show: false
-        },
-        axisLine: {
+          },
+          axisLine: {
             lineStyle: {
-                color: '#A9A9A9',
-                opacity: 0.2,
+              color: "#A9A9A9",
+              opacity: 0.2
             }
-        },
-        axisLabel: {
+          },
+          axisLabel: {
             margin: 10,
             textStyle: {
-                fontSize: 14
+              fontSize: 14
             }
-        },
-        splitLine: {
+          },
+          splitLine: {
             lineStyle: {
-                color: '#A9A9A9',
-                opacity: 0.2,
+              color: "#A9A9A9",
+              opacity: 0.2
             }
+          }
         }
-    }],
-    series: [{
-        type: 'line',
-        smooth: true,
-        symbol: 'circle',
-        symbolSize: 5,
-        showSymbol: false,
-        lineStyle: {
+      ],
+      series: [
+        {
+          type: "line",
+          smooth: true,
+          symbol: "circle",
+          symbolSize: 5,
+          showSymbol: false,
+          lineStyle: {
             normal: {
-                width: 1
+              width: 1
             }
-        },
-        areaStyle: {
+          },
+          areaStyle: {
             normal: {
-                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+              color: new echarts.graphic.LinearGradient(
+                0,
+                0,
+                0,
+                1,
+                [
+                  {
                     offset: 0,
-                    color: 'rgba(105, 19, 167, 0.6)'
-                }, {
+                    color: "rgba(105, 19, 167, 0.6)"
+                  },
+                  {
                     offset: 0.8,
-                    color: 'rgba(105, 19, 167, 0)'
-                }], false),
-                shadowColor: 'rgba(0, 0, 0, 0.4)',
-                shadowBlur: 10
+                    color: "rgba(105, 19, 167, 0)"
+                  }
+                ],
+                false
+              ),
+              shadowColor: "rgba(0, 0, 0, 0.4)",
+              shadowBlur: 10
             }
-        },
-        itemStyle: {
+          },
+          itemStyle: {
             normal: {
-                color: 'rgb(105, 19, 167)',
-                borderColor: 'rgba(137,189,2,0.27)',
-                borderWidth: 12
-
+              color: "rgb(105, 19, 167)",
+              borderColor: "rgba(137,189,2,0.27)",
+              borderWidth: 12
             }
+          },
+          data: [220, 182, 191, 134, 150, 120, 110, 125, 145, 122, 165, 122]
         },
-        data: [220, 182, 191, 134, 150, 120, 110, 125, 145, 122, 165, 122]
-    }, {
-        type: 'line',
-        smooth: true,
-        symbol: 'circle',
-        symbolSize: 5,
-        showSymbol: false,
-        lineStyle: {
+        {
+          type: "line",
+          smooth: true,
+          symbol: "circle",
+          symbolSize: 5,
+          showSymbol: false,
+          lineStyle: {
             normal: {
-                width: 1
+              width: 1
             }
-        },
-        areaStyle: {
+          },
+          areaStyle: {
             normal: {
-                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+              color: new echarts.graphic.LinearGradient(
+                0,
+                0,
+                0,
+                1,
+                [
+                  {
                     offset: 0,
-                    color: 'rgba(45, 222, 255, 0.6)'
-                }, {
+                    color: "rgba(45, 222, 255, 0.6)"
+                  },
+                  {
                     offset: 0.8,
-                    color: 'rgba(45, 222, 255, 0)'
-                }], false),
-                shadowColor: 'rgba(0, 0, 0, 0.1)',
-                shadowBlur: 10
+                    color: "rgba(45, 222, 255, 0)"
+                  }
+                ],
+                false
+              ),
+              shadowColor: "rgba(0, 0, 0, 0.1)",
+              shadowBlur: 10
             }
-        },
-        itemStyle: {
+          },
+          itemStyle: {
             normal: {
-                color: 'rgb(45, 222, 255)',
-                borderColor: 'rgba(0,136,212,0.2)',
-                borderWidth: 12
-
+              color: "rgb(45, 222, 255)",
+              borderColor: "rgba(0,136,212,0.2)",
+              borderWidth: 12
             }
+          },
+          data: [120, 110, 125, 145, 122, 165, 122, 220, 182, 191, 134, 150]
         },
-        data: [120, 110, 125, 145, 122, 165, 122, 220, 182, 191, 134, 150]
-    }, {
-        type: 'line',
-        smooth: true,
-        symbol: 'circle',
-        symbolSize: 5,
-        showSymbol: false,
-        lineStyle: {
+        {
+          type: "line",
+          smooth: true,
+          symbol: "circle",
+          symbolSize: 5,
+          showSymbol: false,
+          lineStyle: {
             normal: {
-                width: 1
+              width: 1
             }
-        },
-        areaStyle: {
+          },
+          areaStyle: {
             normal: {
-                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+              color: new echarts.graphic.LinearGradient(
+                0,
+                0,
+                0,
+                1,
+                [
+                  {
                     offset: 0,
-                    color: 'rgba(123,104,238, 0.9)'
-                }, {
+                    color: "rgba(123,104,238, 0.9)"
+                  },
+                  {
                     offset: 0.8,
-                    color: 'rgba(123,104,238, 0)'
-                }], false),
-                shadowColor: 'rgba(0, 0, 0, 0.1)',
-                shadowBlur: 10
+                    color: "rgba(123,104,238, 0)"
+                  }
+                ],
+                false
+              ),
+              shadowColor: "rgba(0, 0, 0, 0.1)",
+              shadowBlur: 10
             }
-        },
-        itemStyle: {
+          },
+          itemStyle: {
             normal: {
-
-                color: 'rgb(123,104,238)',
-                borderColor: 'rgba(219,50,51,0.2)',
-                borderWidth: 12
+              color: "rgb(123,104,238)",
+              borderColor: "rgba(219,50,51,0.2)",
+              borderWidth: 12
             }
-        },
-        data: [220, 182, 125, 145, 122, 191, 134, 150, 120, 110, 165, 122]
-    }, ]
-};
-  myChart.setOption(option);
+          },
+          data: [220, 182, 125, 145, 122, 191, 134, 150, 120, 110, 165, 122]
+        }
+      ]
+    };
+    myChart.setOption(option);
   }
 };
 </script>
 
 
 <style>
+.cardLine {
+    padding-left: 9px;
+    display: flex;
+    justify-content: space-around;
+}
+.cardBox + .cardBox {
+    margin-left: 15px;
+}
+.cardBox {
+    width: 20%;
+}
 .top-avatar {
   width: 22px;
   height: 22px;
@@ -285,7 +350,7 @@ h1 {
   font-size: 14px;
 }
 .line-chart {
-  width: 1000px;
+  width: 1050px;
   height: 400px;
   margin-left: 30px;
   margin-bottom: 30px;
