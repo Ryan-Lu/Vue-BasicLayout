8dcada92fc96e4a2a0aac9279c788a0d8b4d1fbd
kungua <maguahongkun@outlook.com>
2019-07-21 15:46:44 +0800

    修复一个神奇的bug

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'element-ui/lib/theme-chalk/index.css'
 import './assets/index.scss'
 
 Vue.config.productionTip = false
