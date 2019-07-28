3f642954fa91132101195fbf7dfd079a4dc16094
ryan-lu <229239531@qq.com>
2019-07-28 14:09:02 +0800

    将 signUp 代码复制进入 signUp 并删除掉没有用的代码

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,12 +1,109 @@
 <template>
-  <div class="signInBox">
-      登陆
+  <div class="signUpBox">
+    <div class="formBox">
+      <el-form ref="form" :rules="formRules" :model="form">
+        <el-form-item prop="username">
+          <el-input v-model="form.username" placeholder="请输入账号"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="form.password" type="password" placeholder="请输入密码"></el-input>
+        </el-form-item>
+    
+        <el-form-item>
+          <el-button class="signupBtn" type="primary text" @click="submitForm('form')">登陆</el-button>
+        </el-form-item>
+
+      </el-form>
+      <div class="btnBox">
+        <div class="gray signInLabel">去注册</div>
+        <el-button type="text" class="gray toSignIn">注册</el-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: "",
+      },
+      formRules: {
+        username: [
+          { required: true, message: "你必须输入用户名", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "你必须输入密码", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(form) {
+      this.$refs[form].validate(valid => {
+        if (valid) {
+          // localstorage userList push form
+          let oldUserList = localStorage.getItem("userList");
+          oldUserList = JSON.parse(oldUserList);
+          if (!oldUserList) {
+            oldUserList = [];
+          }
+          //  判断是否重名
+          // 1. 找到系统所有的用户名
+          const nameList = oldUserList.map(item => item.username);
+          const newName = this.form.username;
+
+          if (nameList.includes(newName)) {
+            this.usernameError = true;
+            this.$refs[form].validate();
+            return;
+          }
+          console.log("没找到");
+
+          oldUserList.push(this.form);
+          const newUserList = JSON.stringify(oldUserList);
+          console.log(newUserList);
+          localStorage.setItem("userList", newUserList);
+          // localstorage currentUser = form
+          localStorage.setItem("currentUser", JSON.stringify(this.form));
+          this.$router.push("/app/dashboard");
+        }
+      });
+    }
+  }
 };
 </script>
 
-<style lang="scss"></style>
+
+<style lang="scss" scoped>
+.signUpBox {
+  margin-top: 150px;
+}
+.formBox {
+  background: white;
+  width: 500px;
+  padding: 40px 30px 12px 30px;
+  margin: 20px auto;
+  background: rgb(241, 240, 240);
+  border-radius: 4px;
+  .signupBtn {
+    width: 100%;
+  }
+  .btnBox {
+    margin-left: 4px;
+    .signInLabel {
+      font-size: 14px;
+      color: #aeb0b4;
+    }
+    .toSignIn {
+      &:hover {
+        color: #aeb0b4;
+      }
+      font-size: 13px;
+      font-weight: normal;
+    }
+  }
+}
+</style>
